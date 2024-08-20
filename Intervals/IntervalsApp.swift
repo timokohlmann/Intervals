f47b9b52915cd8753eccb3df100b5edfb3dfe5e8
Timo Kohlmann <kohlmann.timo@gmail.com>
2024-08-20 17:28:25 +0200

    Initial Commit

---
+++ Intervals/IntervalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalsApp.swift
+//  Intervals
+//
+//  Created by Timo Kohlmann on 20.08.24.
+//
+
+import SwiftUI
+
+@main
+struct IntervalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
